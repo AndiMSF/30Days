6265f27e2f3bb385515d8f3c11e6e669e959f6cf
AndiMSF <andimuhammadsatriafadhil77@gmail.com>
2023-08-08 07:23:32 +0700

    fixing delete, post, view post

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+const { ObjectId } = require('mongoose').Types;
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -41,20 +42,21 @@ mongoose.connect('mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PAS
 })
 
 // Buat schema dan collectionnya
-const postSchema = new mongoose.Schema({
-  title: String,
-  content: String
-})
+// const postSchema = new mongoose.Schema({
+//   title: String,
+//   content: String
+// })
 
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String
-})
+  password: String,
+  posts: [{ title: String, content: String }]
+});
 
 //  Passport Local Mongoose
 userSchema.plugin(passportLocalMongoose)
 
-const Post = mongoose.model("Post",postSchema)
+// const Post = mongoose.model("Post",postSchema)
 const User = mongoose.model("User",userSchema)
 
 //  Passport Local Mongoose
@@ -128,18 +130,24 @@ app.post('/register',function(req,res){
 })
 
 // Journal Route
-app.get('/journal',function(req,res){
+app.get('/journal',async function(req,res){
 
-  if(req.isAuthenticated()){
-    Post.find({}, function(err, posts){
+  if(req.isAuthenticated()) {
+    const userId = req.user.id; // Assuming the authenticated user ID is available in req.user._id
+    User.findById(userId, (err,user) => {
+      if(err)
+      {
+        console.log(err);
+      }
+      const userPosts = user.posts;
       res.render("journal", {
-       
-        posts: posts
-        });
-    });
-  } else {
+        posts: userPosts
+      })
+    })
+  }  else {
     res.redirect('/login')
   }
+ 
 })
 
 // userhome route
@@ -150,7 +158,7 @@ app.get('/home',function(req,res){
 // home route
 app.get("/", function(req, res){
 
-  Post.find({}, function(err, posts){
+  User.find({}, function(err, posts){
     res.render("home", {});
   });
 });
@@ -159,32 +167,70 @@ app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
 
+app.post("/compose", function(req, res) {
+  // Assuming the user is already authenticated using Passport.js and Express Session
+  if (req.isAuthenticated()) {
+    // Assuming the user is already authenticated, you can access the user ID from the session or request.
+    const userId = req.user.id; // Assuming the authenticated user ID is available in req.user._id
+    const postData = {
+      title: req.body.postTitle,
+      content: req.body.postBody
+    };
 
-  post.save(function(err){
-    if (!err){
-        res.redirect("/journal");
-    }
-  });
-});
+    User.findById(userId, (err, user) => {
+      if (err) {
+        console.log("Error finding user! " + err);
+      }
 
-app.get("/posts/:postId", function(req, res){
-
-const requestedPostId = req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("post", {
-      title: post.title,
-      content: post.content
+      if (!user) {
+        console.log("No User!");
+      }
+      console.log("Ini data User : "+user);
+      user.posts.push(postData); // Assuming there is a "posts" array field in the User schema
+      user.save((err) => {
+        if (err) {
+          console.log("Error saving post! " + err);
+        } else {
+          res.redirect('/journal')
+          console.log("Post saved successfully!");
+        }
+      });
     });
-  });
-
+  } else {
+    // Handle the case when the user is not authenticated
+    res.redirect("/login"); // Redirect the user to the login page or show an error message
+  }
 });
+
+
+  app.get("/posts/:postId", function (req, res) {
+    User.find(
+      { "_id": ObjectId(req.user.id) },
+      { posts: { $elemMatch: { _id: ObjectId(req.params.postId) } } },
+      function (err, user) {
+        if (err) {
+          // Handle the error here, if necessary
+          console.error(err);
+          return res.status(500).json({ error: "Something went wrong" });
+        }
+  
+        // Check if the user and the posts array exist
+        if (!user || !user[0].posts || user[0].posts.length === 0) {
+          return res.status(404).json({ error: "Post not found" });
+        }
+  
+        // Get the first element of the "posts" array (assuming there's only one post with the given ID)
+        const post = user[0].posts[0];
+        const { title, content } = post;
+
+        res.render("post", {
+          title: title,
+          content: content
+        })
+      }
+    );
+  });
 
 app.get("/about", function(req, res){
   res.render("about", {about: aboutContent});
@@ -194,21 +240,22 @@ app.get("/contact", function(req, res){
   res.render("contact", {contact: contactContent});
 });
 
-app.post('/delete',function(req,res){
-  const checkedItemById = req.body.checkbox 
-  
-  Post.findByIdAndRemove(checkedItemById,function(err){
-    if(!err){
-      console.log("Success Deleted Item");
-      res.redirect('/journal')
-    }
-  })
+app.post('/delete',  function(req,res){
+  const checkedItemById = req.body.checkbox
+  const userId = req.user.id
+ 
+   User.updateOne(
+  { "_id": userId }, // The main document's filter
+  { $pull: { "posts": { "_id": checkedItemById } } }
+).then(() => {
+  res.redirect('/journal')
 })
 
+})
 
-
-
-
+// app.post('/update/:postId',function(req,res){
+//   User.find({"_id":req.user.id},{$set:{"posts.title" : req.body.}})
+// })
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
